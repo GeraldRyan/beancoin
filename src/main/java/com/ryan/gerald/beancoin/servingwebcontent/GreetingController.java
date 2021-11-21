2fef752cdd3e96c3fc7989caa6c8d6bdeb7009f4
GeraldRyan <gerald.ryan40@protonmail.com>
2021-11-20 21:03:50 -0600

    added base url page "

--- src/main/java/com/ryan/gerald/beancoin/servingwebcontent/GreetingController.java
+++ src/main/java/com/ryan/gerald/beancoin/servingwebcontent/GreetingController.java
@@ -4,7 +4,9 @@ package com.ryan.gerald.beancoin.servingwebcontent;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GreetingController {
@@ -13,4 +15,11 @@ public class GreetingController {
         model.addAttribute("name", name);
         return "greeting";
     }
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
 }
