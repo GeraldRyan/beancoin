684680494c6e6edab2d6fd0238c1589ded41aef4
GeraldRyan <gerald.ryan40@protonmail.com>
2021-11-20 20:32:36 -0600

    initialized web app for thymeleaf

---
+++ src/main/java/com/ryan/gerald/beancoin/BeancoinApplication.java
@@ -0,0 +1,13 @@
+package com.ryan.gerald.beancoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeancoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeancoinApplication.class, args);
+	}
+
+}
