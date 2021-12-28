0d85991c1ce7f30cf12f1893d5dab054f331eea9
GeraldRyan <gerald.ryan40@yahoo.com>
2021-12-27 19:08:45 -0600

    starting to add swagger

--- src/main/java/com/ryan/gerald/beancoin/BeancoinApplication.java
+++ src/main/java/com/ryan/gerald/beancoin/BeancoinApplication.java
@@ -1,10 +1,10 @@
 package com.ryan.gerald.beancoin;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BeancoinApplication {
 
 	public static void main(String[] args) {
