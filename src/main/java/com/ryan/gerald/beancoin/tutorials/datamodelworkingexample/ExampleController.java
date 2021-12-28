95039dc1b95b405e6891823c3794645446d2048c
GeraldRyan <gerald.ryan40@yahoo.com>
2021-12-27 22:48:51 -0600

    refactor and clean spring profile properties

--- src/main/java/com/ryan/gerald/beancoin/tutorials/datamodelworkingexample/ExampleController.java
+++ src/main/java/com/ryan/gerald/beancoin/tutorials/datamodelworkingexample/ExampleController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(path="/demo")
-public class MainController {
+public class ExampleController {
     @Autowired private UserExampleRepository userRepository;
 
     @GetMapping(path="/add") // Map ONLY POST Requests
