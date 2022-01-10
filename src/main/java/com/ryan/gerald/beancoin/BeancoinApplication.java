65d453e8b426841af2eb6233bb43c28be9aa4b12
GeraldRyan <gerald.ryan40@yahoo.com>
2022-01-09 20:35:48 -0600

    start exploring mongodb

--- src/main/java/com/ryan/gerald/beancoin/BeancoinApplication.java
+++ src/main/java/com/ryan/gerald/beancoin/BeancoinApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BeancoinApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(BeancoinApplication.class, args);
     }
 }
